278dd43ae00e76addbf96a1e546c2a01d0326795
yusi <44866953+enciyusrina@users.noreply.github.com>
2018-11-09 03:16:18 +0700

    Add files via upload

---
+++ slicing/slicing.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>pendaftaran awr</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+		<!-- One -->
+			<section id="one" class="wrapper style1">
+				<div class="inner" >
+					<article class="feature left" style="background-color:#DEB887; ">
+						<span class="image"><img src="assets/images/tablet.png" alt="" /></span>
+						<div class="content">
+							<H2>STEP 1</h2>
+						  <h3>Klik tombol DAFTAR pada halaman Beranda atau Tentang AWR</h3>
+						</div>
+					</article>
+          <article class="feature left" style="background-color:#DEB887;">
+            <span class="image"><img src="assets/images/smartphone.png" alt=""/></span>
+            <div class="content">
+              <h1> STEP 2 </h1>
+              <H3>Isi form data diri anda secara lengkap dan benar</H3>
+            </div>
+          </article>
+          <article class="feature left" style="background-color:#FFDEAD;">
+            <span class="image"><img src="assets/images/orang.png" alt="" style="margin-left:150px;"/></span>
+            <div class="content">
+              <h2>Allianz Wotd Run 2018</h2>
+              <p>AWR adalah sebuah kegiatan lari yang di lakukan setiap tahun untuk kategori umum dan tahun sekarang merupakan ajang bergensi tersebuat agar masyarakat dapat mengikuti acara tersebut</p>
+              <p style="color:blue;"> ->SHOW LESS DETAIL </p>
+              <p>1. Lari Maraton <br>
+                 2. Lari 500km &nbsp;<br>
+                 3. Lari 1000km &nbsp;
+              </p>
+            </div>
+          </article>
+				</div>
+			</section>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+
+	</body>
+</html>
